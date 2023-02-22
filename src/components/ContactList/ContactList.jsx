2beeb680962624d67d54608c47125a81e5c1b206
PelageyaTelezhynska <p.yu.telezhynska@gmail.com>
2023-02-22 21:43:12 +0300

    fix bugs

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,24 +3,12 @@ import { ContactItem } from 'components/ContactList/ContactItem';
 import { Item } from './ContactList.styled';
 import { selectVisibleContacts } from 'redux/selectors';
 
-// const getFilteredContacts = (contacts, filter) => {
-//   const normalizedFilter = filter.toLowerCase();
-//   return contacts.filter(contact =>
-//     contact.name.toLowerCase().includes(normalizedFilter)
-//   );
-// };
-
 export const ContactList = () => {
   const visibleContacts = useSelector(selectVisibleContacts);
 
-  //   const contacts = useSelector(selectContacts);
-  //   const filter = useSelector(selectFilter);
-
-  //   const visibleContacts = getFilteredContacts(contacts, filter);
-
-  //   if (visibleContacts.length === 0) {
-  //     return null;
-  //   }
+  if (visibleContacts.length === 0) {
+    return null;
+  }
 
   return (
     <ul>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -5,26 +5,6 @@ export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filter;
 
-// export const selectVisibleContacts = state => {
-//   const getFilteredContacts = (contacts, filter) => {
-//     const normalizedFilter = filter.toLowerCase();
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(normalizedFilter)
-//     );
-//   };
-
-//   const contacts = selectContacts(state);
-//   const filter = selectFilter(state);
-
-//   const visibleContacts = getFilteredContacts(contacts, filter);
-
-//   if (visibleContacts.length === 0) {
-//     return contacts;
-//   } else {
-//     return visibleContacts;
-//   }
-// };
-
 export const selectVisibleContacts = createSelector(
   [selectContacts, selectFilter],
   (contacts, filter) => {
@@ -35,11 +15,6 @@ export const selectVisibleContacts = createSelector(
       );
     };
     const visibleContacts = getFilteredContacts(contacts, filter);
-
-    if (visibleContacts.length === 0) {
-      return contacts;
-    } else {
-      return visibleContacts;
-    }
+    return visibleContacts;
   }
 );
